0e5025f06381d7fe75105f97c2bd195f5d5b2825
Eddo Kloosterman <eddo.kloosterman@gmail.com>
2025-08-07 16:16:38 +0200

    Updating webpack config to be compatible with v5

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
+const path = require('path');
+
 module.exports = {
-  mode: "none",
-  entry: "./src/client.js",
-  target: "node",
+  mode: 'none',
+  entry: './src/client.js',
+  target: 'node',
   output: {
-    path: process.env.PWD + "/lib",
-    filename: "dist.js",
-    libraryTarget: "commonjs2"
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'dist.js',
+    library: {
+      type: 'commonjs2'
+    }
   },
   externals: [
-    /^[^\.]/,
+    /^[^\\.]/,
     {
-      "../messages/clientMessage": "../src/messages/clientMessage"
+      '../messages/clientMessage': '../src/messages/clientMessage'
     }
-  ]
-};
+  ],
+  experiments: {
+    topLevelAwait: true
+  }
+}
